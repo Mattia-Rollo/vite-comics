82bbf84b1b8e28433799ee7310f5d10522dca696
Mattia-Rollo <ste.tia@hotmail.it>
2022-11-10 17:15:05 +0100

    add Header Component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-
+    <HeaderComponent />
   </div>
 </template>
 
 <script>
+import HeaderComponent from './components/HeaderComponent.vue';
+
   export default {
-    
-  }
+    components: {
+       HeaderComponent 
+      },
+      data() {
+        return {
+          
+        }
+      },
+}
 </script>
 
 <style lang="scss" scoped>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        ciao sono header
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComponent',
+        data() {
+            return{
+                
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
