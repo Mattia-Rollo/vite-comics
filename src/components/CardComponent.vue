d722d2058b350d2b295291683d8c607e8d3b52ab
mattia.rollo <ste.tia@hotmail.it>
2022-11-14 15:08:37 +0100

    change name vue card

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :key="index" class="card">
+    <a href="#">
+      <img :src="src" :alt="title" />
+      <div>{{ title }}</div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: ["src", "title"],
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  color: antiquewhite;
+  background-color: aqua;
+}
+</style>
